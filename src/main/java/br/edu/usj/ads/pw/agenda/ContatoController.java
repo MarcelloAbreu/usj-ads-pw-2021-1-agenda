3289d898976b6d5330c0c030c1a68f25a58d5100
Marcello <marcellonunes@aluno.usj.edu.br>
2021-05-14 19:56:36 -0300

    main

--- src/main/java/br/edu/usj/ads/pw/agenda/ContatoController.java
+++ src/main/java/br/edu/usj/ads/pw/agenda/ContatoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,7 +31,23 @@ public class ContatoController {
         return modelAndView;
     }
     
+    @GetMapping(value="/detalhes/")
+    public ModelAndView getDetalhes() {
+        // ler contato do banco pelo id 1
+        Contato contato = new Contato();
+        contato = contatoRepository.findById(1L).get();
+
+        // instanciar o template 
+        ModelAndView modelAndView = new ModelAndView("detalhes");
+
+        // popular o template
+        modelAndView.addObject("contato", contato);
+
+        // retornar o template
+        return modelAndView;
+    }
     
+
     @GetMapping(value="/cadastro")
     public ModelAndView getCadastro() {
         ModelAndView modelAndView = new ModelAndView("cadastro");
